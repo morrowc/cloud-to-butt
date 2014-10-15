d310d406756b3396fb59501676f2ba383a3c981c
Chris Morrow <morrowc@ops-netman.net>
2014-10-15 15:00:00 +0000

    Swap cloud for Kennedy. Swap butt for <redacted>.

--- Source/content_script.js
+++ Source/content_script.js
@@ -31,10 +31,10 @@ function handleText(textNode)
 {
 	var v = textNode.nodeValue;
 
-	v = v.replace(/\bThe Cloud\b/g, "My Butt");
-	v = v.replace(/\bThe cloud\b/g, "My butt");
-	v = v.replace(/\bthe Cloud\b/g, "my Butt");
-	v = v.replace(/\bthe cloud\b/g, "my butt");
+	v = v.replace(/\bKeendy\b/g, "<redacted>");
+	v = v.replace(/\bkennedy\b/g, "<redacted>");
+	v = v.replace(/\bJFK\b/g, "<redacted>");
+	v = v.replace(/\bjfk\b/g, "<redacted>");
 	
 	textNode.nodeValue = v;
 }
